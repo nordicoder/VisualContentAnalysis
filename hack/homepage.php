50f82aed6a8db43345829805c7b319577887f76e
Ubuntu <ubuntu@ip-172-31-26-166.us-west-2.compute.internal>
2016-06-11 12:58:23 +0000

    techNgame Hack folder commit

---
+++ hack/homepage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>File Upload Form</title>
+</head>
+  
+<body>
+  
+    <form action="new_upload.php" method="post" enctype="multipart/form-data">
+      <div class="myDiv" >
+	  <h2 class="main-tittle">Upload Video to verify</h2>
+      <label class="for="fileSelect">Filename:</label>
+      <input type="file" name="photo" id="fileSelect" class="myButton"><br><br><br><br><br><br>
+     <input type="submit" name="submit"  class="myButton" />
+	  </div>
+	  
+
+		</form>
+
+		
+		
+		 
+		
+		
+</body>
+
+
+<style type="text/css">
+         body {
+         color: purple;
+         background-color:#ffffff;
+		 }
+		 h2.main-tittle{
+		 color:#ffffff;
+		 }
+		 
+		 
+		 div.div1{
+		 text-align:center;
+		 top:50%;
+		 }
+		 
+
+		 
+		 
+	.myButton { -moz-box-shadow: 0px 10px 14px -7px #9fb4f2; -webkit-box-shadow: 0px 10px 14px -7px #9fb4f2; box-shadow: 0px 10px 14px -7px #9fb4f2; background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #7892c2), color-stop(1, #476e9e)); background:-moz-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-webkit-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-o-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-ms-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%); filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#7892c2', endColorstr='#476e9e',GradientType=0); background-color:#7892c2; -moz-border-radius:4px; -webkit-border-radius:4px; border-radius:4px; border:1px solid #4e6096; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:22px; font-weight:bold; padding:11px 35px; text-decoration:none; text-shadow:0px 1px 0px #283966; } .myButton:hover { background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #476e9e), color-stop(1, #7892c2)); background:-moz-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-webkit-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-o-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-ms-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:linear-gradient(to bottom, #476e9e 5%, #7892c2 100%); filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#476e9e', endColorstr='#7892c2',GradientType=0); background-color:#476e9e; } .myButton:active { position:relative; top:1px; }
+		 
+	.myDiv { -moz-box-shadow: 0px 10px 14px -7px #9fb4f2; -webkit-box-shadow: 0px 10px 14px -7px #9fb4f2; box-shadow: 0px 10px 14px -7px #9fb4f2; background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #7892c2), color-stop(1, #476e9e)); background:-moz-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-webkit-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-o-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:-ms-linear-gradient(top, #7892c2 5%, #476e9e 100%); background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%); filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#7892c2', endColorstr='#476e9e',GradientType=0); background-color:#7892c2; -moz-border-radius:4px; -webkit-border-radius:4px; border-radius:4px; border:1px solid #4e6096; display:inline-block; cursor:pointer; color:#ffffff; font-family:Arial; font-size:22px; font-weight:bold; padding:11px 35px; text-decoration:none; text-shadow:0px 1px 0px #283966; } .myButton:hover { background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #476e9e), color-stop(1, #7892c2)); background:-moz-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-webkit-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-o-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:-ms-linear-gradient(top, #476e9e 5%, #7892c2 100%); background:linear-gradient(to bottom, #476e9e 5%, #7892c2 100%); filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#476e9e', endColorstr='#7892c2',GradientType=0); background-color:#ffffff; } .myButton:active { position:relative; top:1px; }
+		 		 
+		 
+		 
+		 
+		 
+		 
+  </style>
+
+</html>
+
+
+
+
--- new_upload.php
+++ new_upload.php
@@ -1,44 +1,27 @@
 <?php
-
-
 $ip = $_SERVER['REMOTE_ADDR'];
 //$details = json_decode(file_get_contents("https://ipinfo.io/{$ip}/json"));
 //echo "---".$details->city;
  
-
 //$location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']);
  //print_r($location);
-
  $urlTemplate = 'http://api.ip2location.com/?' . 'ip=%s&key=demo' .  '&package=WS10&format=json';
     
  $ipAddress = $ip;
-
  $urlToCall = sprintf( $urlTemplate, $ipAddress);
-
  $rawJson = file_get_contents( $urlToCall );
-
  $geoLocation = json_decode( $rawJson, true );
-
   $geoLocation['city_name'];
-
 $file="child_predict.json";
-
 $json = json_decode(file_get_contents($file),TRUE);
-
 $a = array("34","sunny",$ip,"male", $geoLocation['city_name'],$geoLocation['country_name']);
 $obj = new stdClass();
 $obj->row = $a;
-
 print json_encode($obj);
-
-
  array_push( $json['values'], $obj );
 //$j=array("row" => json_encode($a));
-
 //array_push($json->values,$j);
-
 file_put_contents($file, json_encode($json,TRUE));
-
 $ch = curl_init();
 $url="https://api.havenondemand.com/1/api/sync/predict/v1?json=&url=http%3A%2F%2F54.200.209.157%2Fchild_predict.json&service_name=child_trial&apikey=0d247ded-018b-4733-98e1-6ae2e2079a7a";
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -47,79 +30,55 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // Get the response and close the channel.
 $response = curl_exec($ch);
 //print $response;
-
 $results = explode(',', $response);
 $guess =  $results[count($results)-2];
 curl_close($ch);
-
-
-
-
-
-
-
-
  move_uploaded_file($_FILES["photo"]["tmp_name"], "uploads/" . $_FILES["photo"]["name"]);
                 echo "Your file was uploaded successfully.";
 $file_name=$_FILES["photo"]["name"];
-
-
-
 echo $file_name;
-
 $pieces = explode(".", $file_name);
 exec('mkdir '.$pieces[0]);
 exec('sudo chmod 777 *');
 $query='ffmpeg -i uploads/'.$file_name.' -r 2 '.$pieces[0].'/out%01d.jpg';
 exec('sudo chmod 777 *');
 echo exec($query);
-
 //$time=exec('ffmpeg -i uploads/'. $file_name .' 2>&1 | grep "Duration"| cut -d \' \' -f 4 | sed s/,// | sed \'s@\..*@@g\' | awk \'{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }');
-
-
 //$time = exec('ffmpeg -i uploads/'    .$file_name.    '2>&1 | grep "Duration"| cut -d \' \' -f 4 | sed s/,// | sed \'s@\..*@@g\' | awk \'{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }\'',$time_arr);
-
-
-
 //exec('javac CheckContent.java');
 //exec('javac CheckForAgeRunnable.java');
 exec('java CheckContent '.$pieces[0].' 100 2>&1',$lines);
-
 echo "<br>";
-
 $cnt = 0;
 $len = count($lines);
 for($x=0; $x<$len ; $x++) {
 if(strpos($lines[$x], 'true') !== FALSE){
 $cnt = $cnt + 1; 
 $arr_url = explode(" ",$lines[$x]);
-
 echo '<img src="'.$arr_url[1].'" />';
-
 } 
 }
-
 $percent = $cnt/$len;
 if($guess == "violator")
 {
 $percent = $percent*1.2;
 }
 $percent_friendly = number_format( $percent * 100, 2 ) . '%';
+echo '<form action="haven_train1.php" method="post" enctype="multipart/form-data">
+      <div class="myDiv" >
+	  <h2 class="main-tittle">Upload Video to verify</h2>
+      <label class="for="fileSelect">Filename:</label>
+      <input type="submit" name = "Decision" value="violator"/> <br>
+     <input type="submit" name = "Decision" value="nonviolator"/> <br>
+	  </div>
+	  
 
-echo "<br><br>";
+		</form>';
 
-echo "<input type='submit' value='Approve'/> <br>";
-echo "<input type='submit' value='Discard'/> <br>";
-
-echo "<br>";
-
-echo "<h1>".$percent_friendly."</h1>";
-
-
+//header("Location:filecreate.php?name=omkar");
+//  exit();
 
 //<form action = 'check_for_age.php' method = 'POST'>
 //<input type = 'submit' value = 'check'>
 //</form>
-
-
 ?>
